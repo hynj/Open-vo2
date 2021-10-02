5c50786bd2d8276823df870dfe2fd0388d03c2d1
hynj <thenick112@gmail.com>
2021-10-02 18:11:19 +0100

    2

--- open-vo2/src/main.js
+++ open-vo2/src/main.js
@@ -1,7 +1,22 @@
 import { createApp } from 'vue'
 import { createStore } from 'vuex'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import './index.css'
+import index from '/src/components/Index.vue'
+
+const routes = [
+  {
+      path: '/',
+      name: 'Index',
+      component: index,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
 
 export const store = createStore({
     state () {
@@ -14,6 +29,7 @@ export const store = createStore({
 const app = createApp(App)
 
 app.use(store)
+app.use(router)
 
 
 app.mount('#app')
